b02d07a1aa55096903f9ba00a73376c05b285377
Jonathas-Conceicao <jonathas.conceicao@ossystems.com.br>
2020-03-31 17:10:25 -0300

    Rename crate
    Signed-off-by: Jonathas-Conceicao <jonathas.conceicao@ossystems.com.br>

--- src/bin/dummy.rs
+++ src/bin/dummy.rs
@@ -2,11 +2,11 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-use client_server_testing::{dummy, prelude::*};
+use rwcst::{dummy, prelude::*};
 
 fn main() {
     let local_client = dummy::LocalClient::new();
     let remote_client = dummy::RemoteClient::new("https://foo.bar");
     let app = dummy::App::new(remote_client);
-    client_server_testing::run(local_client, app);
+    rwcst::run(local_client, app);
 }
