ea9fe24d4134a33f7d49ed13850278f6e197e428
asakiz <asakizin@gmail.com>
2020-04-24 09:40:22 -0300

    Add reqwest example
    Signed-off-by: asakiz <asakizin@gmail.com>

---
+++ actix_reqwest/src/main.rs
@@ -0,0 +1,131 @@
+// Copyright (C) 2020 O.S. Systems Sofware LTDA
+//
+// SPDX-License-Identifier: Apache-2.0
+
+use futures_util::lock::Mutex;
+use std::sync::Arc;
+
+use rwcst::prelude::*;
+
+#[actix_rt::main]
+async fn main() {
+    let (url, _guards) = rwcst::start_remote_mock();
+    let local_client = LocalClient::new();
+    let remote_client = RemoteClient::new(&url);
+    let app = App::new(remote_client);
+    rwcst::run(local_client, app).await;
+}
+
+struct LocalClient {
+    client: reqwest::Client,
+}
+
+struct RemoteClient {
+    info: Arc<Mutex<rwcst::Info>>,
+    client: reqwest::Client,
+    remote: String,
+}
+
+struct App {
+    info: Arc<Mutex<rwcst::Info>>,
+    client: RemoteClient,
+}
+
+#[derive(Debug, derive_more::From)]
+enum Err {
+    Server(actix_web::Error),
+    Client(reqwest::Error),
+    Parsing(rwcst::ParsingError),
+}
+type Result<T> = std::result::Result<T, Err>;
+
+#[async_trait::async_trait(?Send)]
+impl rwcst::LocalClientImpl for LocalClient {
+    type Err = Err;
+
+    fn new() -> Self {
+        LocalClient { client: reqwest::Client::new() }
+    }
+
+    async fn fetch_info(&mut self) -> Result<rwcst::Info> {
+        Ok(self.client.get("http://localhost:8001").send().await?.json().await?)
+    }
+}
+
+#[async_trait::async_trait(?Send)]
+impl rwcst::RemoteClientImpl for RemoteClient {
+    type Err = Err;
+
+    fn new(remote: &str) -> Self {
+        RemoteClient {
+            info: Arc::default(),
+            client: reqwest::Client::new(),
+            remote: remote.to_owned(),
+        }
+    }
+
+    async fn fetch_package(&mut self) -> Result<Option<(rwcst::Package, rwcst::Signature)>> {
+        let response = self.client.get(&self.remote).send().await?;
+
+        if let reqwest::StatusCode::OK = response.status() {
+            let sign = rwcst::Signature::from_str(
+                &response.headers().get("Signature").unwrap().to_str().unwrap(),
+            );
+            let pkg = rwcst::Package::parse(&response.bytes().await?)?;
+            return Ok(Some((pkg, sign)));
+        }
+
+        Ok(None)
+    }
+}
+
+#[async_trait::async_trait(?Send)]
+impl rwcst::AppImpl for App {
+    type Err = Err;
+    type RemoteClient = RemoteClient;
+
+    fn new(client: RemoteClient) -> Self {
+        let info = client.info.clone();
+        App { info, client }
+    }
+
+    fn serve(&mut self) -> Result<()> {
+        #[actix_web::get("/")]
+        async fn info(
+            info: actix_web::web::Data<Arc<Mutex<rwcst::Info>>>,
+        ) -> actix_web::HttpResponse {
+            use std::ops::Deref;
+            actix_web::HttpResponse::Ok().json(info.lock().await.deref())
+        }
+
+        let info_ref = self.info.clone();
+        actix_rt::Arbiter::new().exec_fn(|| {
+            actix_rt::Arbiter::spawn(async move {
+                let info_ref = info_ref;
+                actix_web::HttpServer::new(move || {
+                    actix_web::App::new().data(info_ref.clone()).service(info)
+                })
+                .workers(1)
+                .bind("localhost:8001")
+                .unwrap()
+                .run()
+                .await
+                .unwrap();
+            })
+        });
+
+        // Give time for the server to actually start
+        std::thread::sleep(std::time::Duration::from_secs(1));
+
+        Ok(())
+    }
+
+    async fn map_info<F: FnOnce(&mut rwcst::Info)>(&mut self, f: F) -> Result<()> {
+        use std::ops::DerefMut;
+        Ok(f(self.info.lock().await.deref_mut()))
+    }
+
+    async fn client(&mut self) -> Result<&mut RemoteClient> {
+        Ok(&mut self.client)
+    }
+}
